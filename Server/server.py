ecc5dd58383897dca193239b8e4abd5231660284
Joseatorres513 <Joseatorres513>
2024-12-19 18:49:43 -0500

    FSDI 107 Final

--- Server/server.py
+++ Server/server.py
@@ -34,6 +34,16 @@ def get_products():
             products.append(fix_id(prod))
         return json.dumps(products) 
 
+@app.get("/api/catalog")
+def get_catalog():
+    #this if for the catalog portion of the database
+    catalog = []
+    cursor=db.catalog.find({})
+    for cata in cursor:
+        catalog.append(fix_id(cata))
+    return json.dumps(catalog)
+
+
 #this was done in the database portion in Mongo
 def fix_id(obj):
     obj["_id"] = str(obj["_id"])
@@ -48,6 +58,31 @@ def save_product():
         # (fix_id(product)) was added during the database portion
         return json.dumps(fix_id(product))
 
+@app.post("/api/catalog")
+def save_catalog():
+        catalog = request.get_json()
+        print(f"this is my new catalog{catalog}")
+        #catlaog.append(catalog)
+        db.catalog.insert_one(catalog)
+        # (fix_id(catalog)) was added during the database portion
+        return json.dumps(fix_id(catalog))
+
+
+@app.get("/api/reports/total")
+def total_catalog():
+    total = []
+    cursor=db.catalog.find({})
+    for item in cursor:
+        total.append(fix_id(item))
+    return json.dumps(len(total))
+
+@app.get("/api/products/<category>")
+def get_products_by_category(category):
+    products = []
+    cursor = db.products.find({"category": category})
+    for prod in cursor:
+        products.append(fix_id(prod))
+    return json.dumps(products)
 
 @app.put("/api/products/<int:index>")
 def update_product(index):
@@ -72,3 +107,12 @@ def delete_product(index):
         return "That index does not exist"
 
 app.run(debug=True) #that when i save the code, the changes are applied to the server
+
+from bson.objectid import ObjectId
+
+@app.put("/api/products/<id>")
+def update_product(id):
+    updated_product = request.get_json()
+    # Convert string id to ObjectId
+    db.products.update_one({"_id": ObjectId(id)}, {"$set": updated_product})
+    return json.dumps({"status": "updated", "product": updated_product})
